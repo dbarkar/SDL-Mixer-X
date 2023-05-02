18a6a4a57d5365bf9152727d2b26d58d1b2626c8
Denis Barkar <denis.barkar@gmail.com>
2023-05-02 17:01:45 +0400

    CMake: Proper usage of OpusFile and its dependency Opus

--- cmake/find/FindOpusFile.cmake
+++ cmake/find/FindOpusFile.cmake
@@ -1,10 +1,10 @@
-# - Try to find Opus
+# - Try to find OpusFile
 # Once done this will define
-#  Opus_FOUND - System has Opus
-#  Opus_INCLUDE_DIRS - The Opus include directories
-#  Opus_LIBRARIES - The libraries needed to use Opus
+#  OpusFile_FOUND - System has OpusFile
+#  OpusFile_INCLUDE_DIRS - The OpusFile include directories
+#  OpusFile_LIBRARIES - The libraries needed to use OpusFile
 
-find_path(Opus_INCLUDE_DIR "opusfile.h" PATH_SUFFIXES opus)
+find_path(OpusFile_INCLUDE_DIR "opusfile.h" PATH_SUFFIXES opus)
 find_path(Ogg_INCLUDE_DIR "ogg.h" PATH_SUFFIXES ogg)
 
 find_library(Opus_LIBRARY NAMES opus)
@@ -21,13 +21,13 @@ if(Opus_INCLUDE_DIR AND Opus_LIBRARY)
 endif()
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set Opus_FOUND to TRUE
+# handle the QUIETLY and REQUIRED arguments and set OpusFile_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(Opus DEFAULT_MSG
-                                  Opus_LIBRARY Opus_INCLUDE_DIR)
+find_package_handle_standard_args(OpusFile DEFAULT_MSG
+                                  OpusFile_LIBRARY OpusFile_INCLUDE_DIR)
 
 mark_as_advanced(Opus_INCLUDE_DIR Ogg_INCLUDE_DIR Opus_LIBRARY)
 
-set(Opus_LIBRARIES ${OpusFile_LIBRARY} ${Opus_LIBRARY})
-set(Opus_INCLUDE_DIRS ${Ogg_INCLUDE_DIR} ${Opus_INCLUDE_DIR})
+set(OpusFile_LIBRARIES ${OpusFile_LIBRARY} ${Opus_LIBRARY})
+set(OpusFile_INCLUDE_DIRS ${Ogg_INCLUDE_DIR} ${Opus_INCLUDE_DIR})
 
